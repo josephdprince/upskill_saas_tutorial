59aa748f9e32bb6645208d37677601b77873caf1
Joseph Prince <joseph.d.prince@me.com>
2021-08-31 23:42:28 +0000

    Added form validations to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,8 @@
 class Contact < ActiveRecord::Base
     # I dont need to write out the attributes since rails gets it from schema.rb in app/db
+    
+    # This is just making sure that we need a name/email/comment in order to validate data
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :comments, presence: true
 end
